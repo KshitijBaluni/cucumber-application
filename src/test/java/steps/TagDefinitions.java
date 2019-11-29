dcc2b49d0e0e849a857a0eb58cbb8e38b8a1937c
kshitijbaluni <kshitij.baluni@gmail.com>
2019-11-29 12:53:03 +0530

    Tag Def Changes

--- src/test/java/steps/TagDefinitions.java
+++ src/test/java/steps/TagDefinitions.java
@@ -9,93 +9,112 @@ import java.io.*;
 public class TagDefinitions {
 
     @Given("^This is a valid login test$")
-    public void this_is_a_valid_login_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_valid_login_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a valid login");
+
     }
 
     @Given("^This is a invalid login test$")
-    public void this_is_a_invalid_login_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_invalid_login_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a invalid login");
     }
 
     @Given("^This is a contact test case$")
-    public void this_is_a_contact_test_case() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_contact_test_case()throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a contact test case");
     }
 
     @Given("^This is a deal test case$")
-    public void this_is_a_deal_test_case() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_deal_test_case() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a deal test case");
     }
 
     @Given("^This is a tasks test case$")
-    public void this_is_a_tasks_test_case() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_tasks_test_case() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a tasks test case");
     }
 
     @Given("^This is a case test case$")
-    public void this_is_a_case_test_case() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_case_test_case() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a case test case");
     }
 
     @Given("^clicking on left panel links$")
-    public void clicking_on_left_panel_links() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void clicking_on_left_panel_links() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: clicking on left panel links");
     }
 
     @Given("^This is a search deal test$")
-    public void this_is_a_search_deal_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_search_deal_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a search deal test");
     }
 
     @Given("^This is a search contact test$")
-    public void this_is_a_search_contact_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_search_contact_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a search contact test");
     }
 
     @Given("^This is a search case test$")
-    public void this_is_a_search_case_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_search_case_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a search case test");
     }
 
     @Given("^This is a search task test$")
-    public void this_is_a_search_task_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_search_task_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a search task test");
     }
 
     @Given("^This is a search call test$")
-    public void this_is_a_search_call_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_search_call_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a search call test");
     }
 
     @Given("^This is a search email test$")
-    public void this_is_a_search_email_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_search_email_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a search email test");
     }
 
     @Given("^This is a search docs test$")
-    public void this_is_a_search_docs_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_search_docs_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a search docs test");
     }
 
     @Given("^This is a search forms test$")
-    public void this_is_a_search_forms_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_search_forms_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a search forms test");
     }
 
     @Given("^This is a report test$")
-    public void this_is_a_report_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_report_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a report test");
     }
 
     @Given("^This is a logout test$")
-    public void this_is_a_logout_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_logout_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a logout test");
     }
 
     @Given("^This is a close browser test$")
-    public void this_is_a_close_browser_test() {
-        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature");
+    public void this_is_a_close_browser_test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Thread ID :" + Thread.currentThread().getId() + " - Tag Feature: This is a close browser test");
         File file = new File("/home/kshitijbaluni/IdeaProjects/cucumber-application/test.txt");
 
         System.out.println("File name::" + file.getName());
